53b4616138a91f77d60ffc261b4a45091b418520
Felipe Giraldo <fgiraldo@f.giraldo>
2018-11-17 17:22:38 -0500

    Condiciones de frontera periodicas

--- PDE.cpp
+++ PDE.cpp
@@ -4,11 +4,12 @@
 
 void fronteras_fijas();
 void fronteras_abiertas();
+void fronteras_periodicas();
 
 int main(){
 
-	fronteras_abiertas();
-
+	fronteras_fijas();
+	
 	return 0;
 }
 
@@ -24,20 +25,18 @@ void fronteras_fijas(){
 	for(int i=0; i<50; i++){
 		for(int j=0; j<50; j++){
 
-			if(i<19 or i>29){
-				temperatura[i][j] = 10.0;
-			}
-			else{
+			if((i-25)*(i-25)+(j-25)*(j-25) <= 25){
 				temperatura[i][j] = 100.0;
 			}
-			if(i == 0 or i == 49){
+			else{
 				temperatura[i][j] = 10.0;
 			}
-			if((j == 0 or j == 49) and (i<19 or i>29)){
-				temperatura[i][j] = 10.0;
+			if(j!=49){
+				std::cout << temperatura[i][j] << ",";
 			}
-
-			std::cout << temperatura[i][j] << ",";
+			else{
+				std::cout << temperatura[i][j];
+			}
 		}
 
 		std::cout << std::endl;
@@ -48,26 +47,34 @@ void fronteras_fijas(){
 	static float temperatura_futuro[50][50];
 	int contador = 0;	
 	
-	for(int t=0; t<=10000; t++){
+	for(int t=0; t<=100000; t++){
 		for(int i=0; i<50; i++){
 			for(int j=0; j<50; j++){
 
 				temperatura_futuro[i][j] = temperatura[i][j];
-				if(i!=0 and i!=49 and j!=0 and j!=49 and (i<19 or i>29)){
+				if(i!=0 and i!=49 and j!=0 and j!=49 and ((i-25)*(i-25)+(j-25)*(j-25) > 25)){
 					temperatura_futuro[i][j] = (nu*delta_t)*(((temperatura[i+1][j]+temperatura[i-1][j]-(2*temperatura[i][j]))/(delta_x*delta_x))+((temperatura[i][j+1]+temperatura[i][j-1]-(2*temperatura[i][j]))/(delta_y*delta_y))) + temperatura[i][j];
 				}
-				temperatura[i][j] = temperatura_futuro[i][j];
-				if(contador == 2500){
-					std::cout << temperatura[i][j] << ",";
+				if(contador == 25000 and j!=49){
+					std::cout << temperatura_futuro[i][j] << ",";
 				}
+				else if(contador == 25000 and j == 49){
+					std::cout << temperatura_futuro[i][j];
+				}				
 			}
 
-			if(contador == 2500){
+			if(contador == 25000){
 				std::cout << std::endl;
 			}
 		}
 
-		if(contador == 2500){
+		for(int i=0; i<50; i++){
+			for(int j=0; j<50; j++){
+				temperatura[i][j] = temperatura_futuro[i][j];
+			}
+		}
+
+		if(contador == 25000){
 			contador = 0;
 		}
 		else{
@@ -88,13 +95,18 @@ void fronteras_abiertas(){
 	for(int i=0; i<50; i++){
 		for(int j=0; j<50; j++){
 
-			if(i<19 or i>29){
-				temperatura[i][j] = 0.0;
-			}
-			else{
+			if((i-25)*(i-25)+(j-25)*(j-25) <= 25){
 				temperatura[i][j] = 100.0;
 			}
-			std::cout << temperatura[i][j] << ",";
+			else{
+				temperatura[i][j] = 10.0;
+			}
+			if(j!=49){
+				std::cout << temperatura[i][j] << ",";
+			}
+			else{
+				std::cout << temperatura[i][j];
+			}
 		}
 
 		std::cout << std::endl;
@@ -110,28 +122,34 @@ void fronteras_abiertas(){
 			for(int j=0; j<50; j++){
 
 				temperatura_futuro[i][j] = temperatura[i][j];
-				if(i!=0 and i!=49 and j!=0 and j!=49 and (i<19 or i>29)){
+				if(i!=0 and i!=49 and j!=0 and j!=49 and ((i-25)*(i-25)+(j-25)*(j-25) > 25)){
 					temperatura_futuro[i][j] = (nu*delta_t)*(((temperatura[i+1][j]+temperatura[i-1][j]-(2*temperatura[i][j]))/(delta_x*delta_x))+((temperatura[i][j+1]+temperatura[i][j-1]-(2*temperatura[i][j]))/(delta_y*delta_y))) + temperatura[i][j];
 				}
 				for(int k=0; k<50; k++){
-					temperatura_futuro[0][i] = temperatura_futuro[1][i];
-					temperatura_futuro[49][i] = temperatura_futuro[48][i];
+					temperatura_futuro[0][k] = temperatura_futuro[1][k];
+					temperatura_futuro[49][k] = temperatura_futuro[48][k];
 				}
 				for(int l=1; l<49; l++){
 					temperatura_futuro[l][0] = temperatura_futuro[l][1];
 					temperatura_futuro[l][49] = temperatura_futuro[l][48];
 				}
-				temperatura[i][j] = temperatura_futuro[i][j];
-				if(contador == 2500){
-					std::cout << temperatura[i][j] << ",";
+				if(contador == 2500 and j!=49){
+					std::cout << temperatura_futuro[i][j] << ",";
+				}
+				else if(contador == 2500 and j == 49){
+					std::cout << temperatura_futuro[i][j];
 				}
 			}
 
-			if(contador == 2500 and i!=49){
+			if(contador == 2500){
 				std::cout << std::endl;
 			}
 		}
-
+		for(int i=0; i<50; i++){
+			for(int j=0; j<50; j++){
+				temperatura[i][j] = temperatura_futuro[i][j];
+			}
+		}
 		if(contador == 2500){
 			contador = 0;
 		}
@@ -141,4 +159,76 @@ void fronteras_abiertas(){
 	}
 }
 	
+void fronteras_periodicas(){
 
+	static float temperatura[50][50];
+	float delta_x = 1.0;
+	float delta_y = 1.0;
+	float nu = 1.62/(820*0.0271);
+	float delta_t = 0.1;
+
+	//condiciones iniciales
+	for(int i=0; i<50; i++){
+		for(int j=0; j<50; j++){
+
+			if((i-25)*(i-25)+(j-25)*(j-25) <= 25){
+				temperatura[i][j] = 100.0;
+			}
+			else{
+				temperatura[i][j] = 10.0;
+			}
+			if(j!=49){
+				std::cout << temperatura[i][j] << ",";
+			}
+			else{
+				std::cout << temperatura[i][j];
+			}
+		}
+
+		std::cout << std::endl;
+	}
+
+	//iteraciones
+
+	static float temperatura_futuro[50][50];
+	int contador = 0;	
+	
+	for(int t=0; t<=10000; t++){
+		for(int i=0; i<50; i++){
+			for(int j=0; j<50; j++){
+
+				temperatura_futuro[i][j] = temperatura[i][j];
+				if(i!=0 and i!=49 and j!=0 and j!=49 and ((i-25)*(i-25)+(j-25)*(j-25) > 25)){
+					temperatura_futuro[i][j] = (nu*delta_t)*(((temperatura[i+1][j]+temperatura[i-1][j]-(2*temperatura[i][j]))/(delta_x*delta_x))+((temperatura[i][j+1]+temperatura[i][j-1]-(2*temperatura[i][j]))/(delta_y*delta_y))) + temperatura[i][j];
+				}
+				for(int k=0; k<50; k++){
+					temperatura_futuro[0][k] = temperatura_futuro[49][k];
+				}
+				for(int l=1; l<49; l++){
+					temperatura_futuro[l][0] = temperatura_futuro[l][49];
+				}
+				if(contador == 2500 and j!=49){
+					std::cout << temperatura_futuro[i][j] << ",";
+				}
+				else if(contador == 2500 and j == 49){
+					std::cout << temperatura_futuro[i][j];
+				}
+			}
+
+			if(contador == 2500){
+				std::cout << std::endl;
+			}
+		}
+		for(int i=0; i<50; i++){
+			for(int j=0; j<50; j++){
+				temperatura[i][j] = temperatura_futuro[i][j];
+			}
+		}
+		if(contador == 2500){
+			contador = 0;
+		}
+		else{
+			contador = contador + 1;
+		}
+	}
+}
