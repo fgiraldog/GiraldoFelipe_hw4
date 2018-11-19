c5b146d15749c51376d92ab5f5702cd487f0266b
Felipe Giraldo <fgiraldo@f.giraldo>
2018-11-18 21:17:52 -0500

    Guardado de datos corregidos

--- ODE.cpp
+++ ODE.cpp
@@ -1,41 +1,59 @@
 #include <iostream>
 #include <stdio.h>
 #include <cmath>
+#include <math.h> 
+#include <fstream>
+#include <sstream>
 
 float g = 10.0;
 float c = 0.2;
 float m = 0.2;
 float h = 0.001;
+float max_x = 0.0;
+float max_angulo = 0.0;
 float vx_prime(float v_x, float v_y);
 float vy_prime(float v_x, float v_y);
-void iteraciones(float theta);
+void iteraciones(float theta, int si);
 
 int main(){
+	
+	iteraciones(45*3.1415/180,1);
+	iteraciones(10*3.1415/180,0);
+	iteraciones(20*3.1415/180,0);
+	iteraciones(30*3.1415/180,0);
+	iteraciones(40*3.1415/180,0);
+	iteraciones(50*3.1415/180,0);
+	iteraciones(60*3.1415/180,0);
+	iteraciones(70*3.1415/180,0);
 
-	iteraciones(3.1415/4);
+	std::cout << "El angulo para el cual la distancia recorrida es mayor es: " << max_angulo << " grados " << std::endl;
+
 	return 0;
 }
 
 float vx_prime(float v_x, float v_y){
 	
-	return (-c*(v_x)*((v_x*v_x)+(v_y*v_y)))/(m*sqrt((v_x*v_x)+(v_y*v_y)));
+	return (-c* (v_x)*((v_x*v_x)+(v_y*v_y)))/(m*sqrt((v_x*v_x)+(v_y*v_y)));
 
 }
 
 float vy_prime(float v_x, float v_y){
 
-	return (-g -(c*(v_y)*((v_x*v_x)+(v_y*v_y))/(m*sqrt((v_x*v_x)+(v_y*v_y)))));
+	return -g -(c*(v_y)*((v_x*v_x)+(v_y*v_y))/(m*sqrt((v_x*v_x)+(v_y*v_y))));
 
-}
+ }
 
-void iteraciones(float theta){
+void iteraciones(float theta, int si){
+
+	std::ofstream datos;
+	datos.open("ODE.dat", std::ios::out | std::ios::app);
 
 	float t_0 = 0.0;
 	float x_0 = 0.0;
 	float y_0 = 0.0;
 	float theta_0 = theta;
 	float vx_0 = 300*cos(theta_0);
-	float vy_0 = 300*cos(theta_0);
+	float vy_0 = 300*sin(theta_0);
 
 	// Leap frog inicializacion
 	
@@ -45,7 +63,7 @@ void iteraciones(float theta){
 	float x_ini = x_0;
 	float y_ini = y_0;
 
-	std::cout << t_0 << "," << x_ini << "," << y_ini << "," << t_half << "," << vx_thalf << "," << vy_thalf << std::endl; 
+	datos << t_0 << "," << x_ini << "," << y_ini << "," << t_half << "," << vx_thalf << "," << vy_thalf << std::endl; 
 
 	for(int i=1; i<10000; i++){
 		
@@ -62,15 +80,28 @@ void iteraciones(float theta){
 		y_ini = y_siguiente;
 
 		float t_pos = i*h;
-		float t_vel = t_pos - h;
+		float t_vel = t_pos - h/2;
 		
 		if(y_ini < 0.1 and i > 10){
 			
 			i = 10000;
 		}
 
-		std::cout << t_pos << "," << x_ini << "," << y_ini << "," << t_vel << "," << vx_thalf << "," << vy_thalf << std::endl;
+		datos << t_pos << "," << x_ini << "," << y_ini << "," << t_vel << "," << vx_thalf << "," << vy_thalf << std::endl;
+		
+		if(si == 1 and i == 10000){
+			
+			std::cout << "La distancia recorrida por el proyectil (con un angulo_0 = 45) es de: " << x_ini << "m" << std::endl;
+		}
+		
+		if(x_ini > max_x and i == 10000){
+		
+			max_x = x_ini;
+			max_angulo = theta_0*180/3.1415;
+		}
 	
 	}
+
+	datos.close();
 }
 
