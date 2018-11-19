e6092a8585f12c4a775d554f379b0492b5039f9d
Felipe Giraldo <fgiraldo@f.giraldo>
2018-11-19 12:32:05 -0500

    Version final con comentarios

--- PDE.cpp
+++ PDE.cpp
@@ -3,9 +3,10 @@
 #include <cmath>
 #include <fstream>
 
-void fronteras_fijas();
-void fronteras_abiertas();
-void fronteras_periodicas();
+//Declaracion de las funciones
+void fronteras_fijas(); //Esta funcion calcula las iteraciones para el caso de las fronteras fijas
+void fronteras_abiertas(); //Esta funcion calcula las iteraciones para el caso de las fronteras abiertas
+void fronteras_periodicas(); //Esta funcion calcula las iteraciones para el caso de las fronteras periodicas
 
 int main(){
 	
@@ -17,17 +18,18 @@ int main(){
 }
 
 void fronteras_fijas(){
-
+	//Esta linea de codigo lo que hace es abrir el archivo PDE.day y guardar las iteraciones para este metodo
 	std::ofstream datos;
-	datos.open("PDE.dat", std::ios::out | std::ios::app);
+	datos.open("PDE.dat", std::ios::out | std::ios::app); //El ios ayuda a guardar los datos y no sobreescribirlos
 
+	//Definicion de las variables
 	static float temperatura[50][50];
 	float delta_x = 0.01;
 	float delta_y = 0.01;
 	float nu = 1.62/(820*2710);
-	float delta_t = 0.1*delta_x*delta_y/nu;
+	float delta_t = 0.1*delta_x*delta_y/nu; //Para estabilidad numerica se necesita que delta t = 0.5*delta_x*delta_y/nu, pero para asegurar, se uso 0.1
 
-	//condiciones iniciales
+	//Condiciones iniciales
 	for(int i=0; i<50; i++){
 		for(int j=0; j<50; j++){
 
@@ -47,13 +49,13 @@ void fronteras_fijas(){
 
 		datos << std::endl;
 		float promedio_0 = 0.0;
-		for(int i=0; i<50; i++){
-			for(int j=0; j<50; j++){
-				promedio_0 = promedio_0 + temperatura[i][j];
+		for(int h=0; h<50; h++){
+			for(int a=0; a<50; a++){
+				promedio_0 = promedio_0 + temperatura[h][a]; //Calculo del promedio
 			}
 		}
 		if(i == 49){
-			datos << 0 << "," << promedio_0/250.0;
+			datos << 0 << "," << promedio_0/2500.0; //Calculo del promedio
 			
 			for(int m=0; m<49; m++){
 				if(m != 48){
@@ -66,17 +68,17 @@ void fronteras_fijas(){
 		}
 	}
 
-	//iteraciones
-
+	//Iteraciones, para este caso se uso forward difference para poder calcular las iteraciones. Esto se debe a que si se usaba central difference, para que hubiera estabilidad numerica, el delta 	x tenia que ser demasiado pequenho
+	//Declaracion de variables
 	static float temperatura_futuro[50][50];
 	int contador = 0;	
 	
-	for(int t=0; t<=1000; t++){
-		for(int i=0; i<50; i++){
-			for(int j=0; j<50; j++){
+	for(int t=0; t<=1000; t++){ //For para el tiempo
+		for(int i=0; i<50; i++){ //For para las filas
+			for(int j=0; j<50; j++){ //For para las columnas
 
 				temperatura_futuro[i][j] = temperatura[i][j];
-				if(i!=0 and i!=49 and j!=0 and j!=49 and ((i-25)*(i-25)+(j-25)*(j-25) > 25)){
+				if(i!=0 and i!=49 and j!=0 and j!=49 and ((i-25)*(i-25)+(j-25)*(j-25) > 25)){ //El for no se corre en las fronteras para que estas siempre sean fijas
 					temperatura_futuro[i][j] = (nu*delta_t)*(((temperatura[i+1][j]+temperatura[i-1][j]-(2*temperatura[i][j]))/(delta_x*delta_x))+((temperatura[i][j+1]+temperatura[i][j-1]-(2*temperatura[i][j]))/(delta_y*delta_y))) + temperatura[i][j];
 				}
 				if(contador == 250 and j!=49){
@@ -91,13 +93,13 @@ void fronteras_fijas(){
 				datos << std::endl;
 				
 				float promedio = 0.0;
-				for(int i=0; i<50; i++){
-					for(int j=0; j<50; j++){
-						promedio = promedio + temperatura_futuro[i][j];
+				for(int h=0; h<50; h++){
+					for(int a=0; a<50; a++){
+						promedio = promedio + temperatura_futuro[h][a]; //Calculo del promedio
 					}
 				}
 				if(i == 49){
-					datos << t*delta_t << "," << promedio/250.0;
+					datos << t*delta_t << "," << promedio/2500.0; //Calculo del promedio
 
 					for(int m=0; m<49; m++){
 						if(m != 48){
@@ -111,13 +113,13 @@ void fronteras_fijas(){
 			}
 		}
 
-		for(int i=0; i<50; i++){
-			for(int j=0; j<50; j++){
-				temperatura[i][j] = temperatura_futuro[i][j];
+		for(int l=0; l<50; l++){
+			for(int p=0; p<50; p++){
+				temperatura[l][p] = temperatura_futuro[l][p];
 			}
 		}
 
-		if(contador == 250){
+		if(contador == 250){ //Se trabaja con un contador para poder imprimir iteraciones especificas
 			contador = 0;
 		}
 		else{
@@ -125,21 +127,22 @@ void fronteras_fijas(){
 		}
 	}
 
-	datos.close();
+	datos.close(); //Esta linea cierra el archivo abierto anteriormente
 }
 
 void fronteras_abiertas(){
-
+	//Esta linea de codigo lo que hace es abrir el archivo PDE.day y guardar las iteraciones para este metodo
 	std::ofstream datos;
-	datos.open("PDE.dat", std::ios::out | std::ios::app);
+	datos.open("PDE.dat", std::ios::out | std::ios::app); //El ios ayuda a guardar los datos y no sobreescribirlos
 
+	//Definicion de las variables
 	static float temperatura[50][50];
 	float delta_x = 0.01;
 	float delta_y = 0.01;
 	float nu = 1.62/(820*2710);
-	float delta_t = 0.1*delta_x*delta_y/nu;
+	float delta_t = 0.1*delta_x*delta_y/nu; //Para estabilidad numerica se necesita que delta t = 0.5*delta_x*delta_y/nu, pero para asegurar, se uso 0.1
 
-	//condiciones iniciales
+	//Condiciones iniciales
 	for(int i=0; i<50; i++){
 		for(int j=0; j<50; j++){
 
@@ -159,13 +162,13 @@ void fronteras_abiertas(){
 
 		datos << std::endl;
 		float promedio_0 = 0.0;
-		for(int i=0; i<50; i++){
-			for(int j=0; j<50; j++){
-				promedio_0 = promedio_0 + temperatura[i][j];
+		for(int h=0; h<50; h++){
+			for(int a=0; a<50; a++){
+				promedio_0 = promedio_0 + temperatura[h][a]; //Calculo del promedio
 			}
 		}
 		if(i == 49){
-			datos << 0 << "," << promedio_0/250.0;
+			datos << 0 << "," << promedio_0/2500.0; //Calculo del promedio
 
 			for(int m=0; m<49; m++){
 				if(m != 48){
@@ -178,14 +181,14 @@ void fronteras_abiertas(){
 		}
 	}
 
-	//iteraciones
-
+	//Iteraciones, para este caso se uso forward difference para poder calcular las iteraciones. Esto se debe a que si se usaba central difference, para que hubiera estabilidad numerica, el delta 	x tenia que ser demasiado pequenho
+	//Declaracion de variables
 	static float temperatura_futuro[50][50];
 	int contador = 0;	
 	
-	for(int t=0; t<=1000; t++){
-		for(int i=0; i<50; i++){
-			for(int j=0; j<50; j++){
+	for(int t=0; t<=1000; t++){ //For para el tiempo
+		for(int i=0; i<50; i++){ //For para las lineas
+			for(int j=0; j<50; j++){ //For para las columnas
 
 				temperatura_futuro[i][j] = temperatura[i][j];
 				if(i!=0 and i!=49 and j!=0 and j!=49 and ((i-25)*(i-25)+(j-25)*(j-25) > 25)){
@@ -203,13 +206,13 @@ void fronteras_abiertas(){
 				datos << std::endl;
 				
 				float promedio = 0.0;
-				for(int i=0; i<50; i++){
-					for(int j=0; j<50; j++){
-						promedio = promedio + temperatura_futuro[i][j];
+				for(int h=0; h<50; h++){
+					for(int a=0; a<50; a++){
+						promedio = promedio + temperatura_futuro[h][a]; //Calculo del promedio
 					}
 				}
 				if(i == 49){
-					datos << t*delta_t << "," << promedio/250.0;
+					datos << t*delta_t << "," << promedio/2500.0; //Calculo del promedio
 					for(int m=0; m<49; m++){
 						if(m != 48){
 							datos << 0 << ",";
@@ -222,19 +225,19 @@ void fronteras_abiertas(){
 			}
 		}
 		for(int k=0; k<50; k++){
-			temperatura_futuro[0][k] = temperatura_futuro[1][k];
+			temperatura_futuro[0][k] = temperatura_futuro[1][k]; //Condiciones para fronteras abiertas
 			temperatura_futuro[49][k] = temperatura_futuro[48][k];
 		}
 		for(int l=1; l<49; l++){
-			temperatura_futuro[l][0] = temperatura_futuro[l][1];
+			temperatura_futuro[l][0] = temperatura_futuro[l][1]; //Condiciones para fronteras abiertas
 			temperatura_futuro[l][49] = temperatura_futuro[l][48];
 		}
-		for(int i=0; i<50; i++){
-			for(int j=0; j<50; j++){
-				temperatura[i][j] = temperatura_futuro[i][j];
+		for(int y=0; y<50; y++){
+			for(int q=0; q<50; q++){
+				temperatura[y][q] = temperatura_futuro[y][q];
 			}
 		}
-		if(contador == 250){
+		if(contador == 250){ //Se trabaja con un contador para poder imprimir iteraciones especificas
 			contador = 0;
 		}
 		else{
@@ -242,21 +245,22 @@ void fronteras_abiertas(){
 		}
 	}
 
-	datos.close();
+	datos.close(); //Esta linea cierra el archivo abierto anteriormente
 }
 	
 void fronteras_periodicas(){
-
+	//Esta linea de codigo lo que hace es abrir el archivo PDE.day y guardar las iteraciones para este metodo
 	std::ofstream datos;
-	datos.open("PDE.dat", std::ios::out | std::ios::app);
+	datos.open("PDE.dat", std::ios::out | std::ios::app); //El ios ayuda a guardar los datos y no sobreescribirlos
 
+	//Definicion de las variables
 	static float temperatura[50][50];
 	float delta_x = 0.01;
 	float delta_y = 0.01;
 	float nu = 1.62/(820*2710);
-	float delta_t = 0.1*delta_x*delta_y/nu;
+	float delta_t = 0.1*delta_x*delta_y/nu; //Para estabilidad numerica se necesita que delta t = 0.5*delta_x*delta_y/nu, pero para asegurar, se uso 0.1
 
-	//condiciones iniciales
+	//Condiciones iniciales
 	for(int i=0; i<50; i++){
 		for(int j=0; j<50; j++){
 
@@ -276,13 +280,13 @@ void fronteras_periodicas(){
 
 		datos << std::endl;
 		float promedio_0 = 0.0;
-		for(int i=0; i<50; i++){
-			for(int j=0; j<50; j++){
-				promedio_0 = promedio_0 + temperatura[i][j];
+		for(int h=0; h<50; h++){
+			for(int a=0; a<50; a++){
+				promedio_0 = promedio_0 + temperatura[h][a]; //Calculo del promedio
 			}
 		}
 		if(i == 49){
-			datos << 0 << "," << promedio_0/250.0;
+			datos << 0 << "," << promedio_0/2500.0; //Calculo del promedio
 			for(int m=0; m<49; m++){
 				if(m != 48){
 					datos << 0 << ",";
@@ -294,14 +298,14 @@ void fronteras_periodicas(){
 		}
 	}
 
-	//iteraciones
-
+	//Iteraciones, para este caso se uso forward difference para poder calcular las iteraciones. Esto se debe a que si se usaba central difference, para que hubiera estabilidad numerica, el delta 	x tenia que ser demasiado pequenho
+	//Declaracion de variables
 	static float temperatura_futuro[50][50];
 	int contador = 0;	
 	
-	for(int t=0; t<=1000; t++){
-		for(int i=0; i<50; i++){
-			for(int j=0; j<50; j++){
+	for(int t=0; t<=1000; t++){ //For para el tiempo 
+		for(int i=0; i<50; i++){ //For para las lineas
+			for(int j=0; j<50; j++){ //For para las columnas
 
 				temperatura_futuro[i][j] = temperatura[i][j];
 				if(i!=0 and j!=0 and ((i-25)*(i-25)+(j-25)*(j-25) > 25)){
@@ -319,13 +323,13 @@ void fronteras_periodicas(){
 				datos << std::endl;
 				
 				float promedio = 0.0;
-				for(int i=0; i<50; i++){
-					for(int j=0; j<50; j++){
-						promedio = promedio + temperatura_futuro[i][j];
+				for(int h=0; h<50; h++){
+					for(int a=0; a<50; a++){
+						promedio = promedio + temperatura_futuro[h][a]; //Calculo del promedio
 					}
 				}
 				if(i == 49){
-					datos << t*delta_t << "," << promedio/250.0;
+					datos << t*delta_t << "," << promedio/2500.0; //Calculo del promedio
 					for(int m=0; m<49; m++){
 						if(m != 48){
 							datos << 0 << ",";
@@ -338,17 +342,17 @@ void fronteras_periodicas(){
 			}
 		}
 		for(int k=0; k<50; k++){
-			temperatura_futuro[0][k] = temperatura_futuro[49][k];
+			temperatura_futuro[0][k] = temperatura_futuro[48][k]; //Condiciones para fronteras periodicas
 		}
 		for(int l=0; l<50; l++){
-			temperatura_futuro[l][0] = temperatura_futuro[l][49];
+			temperatura_futuro[l][0] = temperatura_futuro[l][48]; //Condiciones para fronteras periodicas
 		}
-		for(int i=0; i<50; i++){
-			for(int j=0; j<50; j++){
-				temperatura[i][j] = temperatura_futuro[i][j];
+		for(int y=0; y<50; y++){
+			for(int q=0; q<50; q++){
+				temperatura[y][q] = temperatura_futuro[y][q];
 			}
 		}
-		if(contador == 250){
+		if(contador == 250){ //Se trabaja con un contador para poder imprimir iteraciones especificas
 			contador = 0;
 		}
 		else{
@@ -356,5 +360,5 @@ void fronteras_periodicas(){
 		}
 	}
 
-	datos.close();
+	datos.close(); //Esta linea cierra el archivo abierto anteriormente
 }
