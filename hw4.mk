d99277700a2df5f631663a6ccd371e2045779629
Felipe Giraldo <fgiraldo@f.giraldo>
2018-11-19 11:38:57 -0500

    Correccion de dependencias

--- hw4.mk
+++ hw4.mk
@@ -1,8 +1,14 @@
 Resultados_hw4.pdf:Resultados_hw4.tex ODE_45.pdf ODE_todos.pdf PDE_abiertas.pdf PDE_fijas.pdf PDE_periodicas.pdf
 	pdflatex Resultados_hw4.tex
 
-ODE_45.pdf ODE_todos.pdf PDE_abiertas.pdf PDE_fijas.pdf PDE_periodicas.pdf:Plots_hw4.py PDE.o ODE.o
+ODE_45.pdf ODE_todos.pdf PDE_abiertas.pdf PDE_fijas.pdf PDE_periodicas.pdf:Plots_hw4.py PDE.dat ODE.dat
 	python Plots_hw4.py
+
+PDE.dat:PDE.o
+	./PDE.o
+
+ODE.dat:ODE.o
+	./ODE.o
 
 PDE.o:PDE.cpp
 	g++ -o PDE.o PDE.cpp
